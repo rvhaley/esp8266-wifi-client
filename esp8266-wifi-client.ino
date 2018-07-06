b90ba5678ea2bf3eb8562c0eb0bcddd9fb766557
Richard Haley III <rvhaley@hotmail.com>
2018-07-06 14:27:13 -0700

    Moved disconnect code outside of loop() function.

--- esp8266-wifi-client.ino
+++ esp8266-wifi-client.ino
@@ -6,29 +6,31 @@ extern "C" {
 }
 
 /* Replace values below with your network credentials */
-char sta_ssid[] = "SSID"; // Network SSID
-char sta_pass[] = "PASSWORD"; // Network password
+char sta_ssid[] = ""; // Network SSID
+char sta_pass[] = ""; // Network password
 
 void setup() {
-  Serial.begin(74880);
-  Serial.setDebugOutput(true);
+  Serial1.begin(74880);
+  Serial1.setDebugOutput(true);
 
   WiFi.persistent(false);
   WiFi.mode(WIFI_STA);
-  
-  Serial.print("Connecting to network ");
-  Serial.println(sta_ssid);
-  
+ 
+  Serial1.print("Connecting to network ");
+  Serial1.println(sta_ssid);
+   
   WiFi.begin(sta_ssid, sta_pass);
   while (WiFi.status() != WL_CONNECTED) {
         delay(500);
   }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    delay(10000);
+    Serial1.println("Disconnecting...");
+    wifi_station_disconnect();
+  }
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-    delay(10000);
-    Serial.println("Disconnecting...");
-    wifi_station_disconnect();
-  }
+  
 }
